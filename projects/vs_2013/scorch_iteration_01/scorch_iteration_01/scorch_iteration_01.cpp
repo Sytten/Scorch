d72671b427ebc8ac35ada423e6d842ea6daa26fa
Jean-Philippe Fournier <fournierje.info@gmail.com>
2016-02-24 11:34:48 -0500

    Ajout de fichiers VS

--- projects/vs_2013/scorch_iteration_01/scorch_iteration_01/scorch_iteration_01.cpp
+++ projects/vs_2013/scorch_iteration_01/scorch_iteration_01/scorch_iteration_01.cpp
@@ -2,15 +2,24 @@
 //
 
 #include "stdafx.h"
-
+#include <string>
 #include "Game.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Game game();
+	CannonBall * cannonBall = new CannonBall();
+	Castle castle;
 
+	Cannon cannon;
 
+	cannonBall->collide(castle);
+	cannonBall->collide(castle); 
+	cannonBall->collide(castle);
+	cannonBall->collide(castle);
+	cannonBall->collide(castle);
+
+	std::cin.get();
 
 	return 0;
 }

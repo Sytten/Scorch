65e969ad41f5e0c935ced87853ff71252fc046ab
Emile Fugulin <emilefugulin@hotmail.com>
2016-02-24 16:24:46 -0500

    Add implementation for the collision checker

--- include/CollisionChecker.h
+++ include/CollisionChecker.h
@@ -3,20 +3,20 @@
 
 #include <memory>
 
-#include "Entities/Entity.h"
+#include "Entities/PhysicalEntity.h"
 
 class CollisionChecker
 {
     public:
         CollisionChecker();
 
-       // static bool collide(Entity& p_entity);
+        static bool checkCollisions(PhysicalEntity& p_entity);
 
-       // static bool registerEntity(std::weak_ptr<Entity> p_newEntity);
-       // static bool unregisterEntity(int p_oldEntityID);
+        static bool registerEntity(std::weak_ptr<PhysicalEntity> p_newEntity);
+        //static bool unregisterEntity(int p_oldEntityID);
 
     private:
-       // static vector<std::weak_ptr<Entity> > m_entities;
+        static vector<std::weak_ptr<PhysicalEntity> > m_entities;
 };
 
 #endif // COLLISIONCHECKER_H
--- src/CollisionChecker.cpp
+++ src/CollisionChecker.cpp
@@ -4,3 +4,37 @@ CollisionChecker::CollisionChecker()
 {
     //ctor
 }
+
+bool CollisionChecker::checkCollisions(PhysicalEntity& p_entity)
+{
+#ifdef GAME_DEBUG
+    static int numberOfCalls = 0;
+
+    if(numberOfCalls > 1)
+    {
+        for(int i = 0; i < m_entities.size(); i++)
+        {
+            auto entity = m_entities[i].lock();
+            if(entity)
+            {
+                if(entity->getID() == 1002)
+                {
+                    entity->collide(p_entity);
+                    p_entity->collide(entity);
+                }
+            }
+            else
+                m_entities.erase(m_entities.begin()+i);
+        }
+    }
+
+    numberOfCalls++;
+#endif
+
+}
+
+bool CollisionChecker::registerEntity(std::weak_ptr<PhysicalEntity> p_newEntity)
+{
+    m_entities.push_back(p_newEntity);
+    return true;
+}

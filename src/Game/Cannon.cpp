3d04458d8c663633f892f39e0f8982d4bbcf47ce
Emile Fugulin <emilefugulin@hotmail.com>
2016-04-03 22:34:16 -0400

    Add functions to fire cannonball

--- src/Game/Cannon.cpp
+++ src/Game/Cannon.cpp
@@ -1,6 +1,6 @@
 #include "Game/Cannon.h"
 
-Cannon::Cannon(const QPixmap & cannon, const QPixmap & base, QPointF cannonRelativePosition, Player owner, QGraphicsItem * parent) : Entity(QPixmap(), owner, parent), m_power(50), m_angle(0)
+Cannon::Cannon(const QPixmap & cannon, const QPixmap & base, QPointF cannonRelativePosition, bool reversed, Player owner, QGraphicsItem * parent) : Entity(QPixmap(), owner, parent), m_power(50), m_angle(0), m_reversed(reversed)
 {
 	m_cannon = new QGraphicsPixmapItem(cannon, this);
 	m_base = new QGraphicsPixmapItem(base, this);
@@ -73,6 +73,23 @@ void Cannon::reset()
 {
 	m_angle = 0;
 	m_power = 50;
+	m_cannon->setRotation(0);
 	emit powerChanged(m_power);
 	emit angleChanged(m_angle);
+}
+
+CannonBall * Cannon::fire()
+{
+	double power = m_power;
+
+	if (m_reversed)
+		power *= -1;
+
+	emit fired();
+	return new CannonBall(QPixmap(":/resources/cannonball.png"), QVector2D(power * 2 * cos(toRadians(m_angle)), m_power * 2 * sin(toRadians(m_angle))), QVector2D(m_cannon->mapToScene(m_cannon->boundingRect().right(), m_cannon->boundingRect().top())), owner());
+}
+
+double Cannon::toRadians(double degrees)
+{
+	return degrees * 3.14159265 / 180.0;
 }

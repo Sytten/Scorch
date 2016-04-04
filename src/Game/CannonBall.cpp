c5239dd1e11e59e8cca798590a43d22594b9d1b5
Emile Fugulin <emilefugulin@hotmail.com>
2016-04-03 22:36:03 -0400

    Modify cannonball to be fired and do something

--- src/Game/CannonBall.cpp
+++ src/Game/CannonBall.cpp
@@ -1,16 +1,17 @@
 #include "Game/CannonBall.h"
 
-CannonBall::CannonBall(const QPixmap &pixmap, Player owner, QGraphicsItem *parent) : Entity(pixmap, owner, parent)
+CannonBall::CannonBall(const QPixmap & pixmap, QVector2D velocity, QVector2D position, Player owner, QGraphicsItem * parent) : Entity(pixmap, owner, parent), m_initialVelocity(velocity), m_initialPosition(position), m_gravity(9.8), m_timeSinceShot(0)
 {
-
+	setScale(0.1);
+	setPos(m_initialPosition.toPointF());
 }
 
 void CannonBall::updateEntity(double deltaTime)
 {
+	m_timeSinceShot += deltaTime/1000.0;
 
-}
+	double x = m_initialPosition.x() + m_initialVelocity.x() * m_timeSinceShot;
+	double y = m_initialPosition.y() - m_initialVelocity.y() * m_timeSinceShot + 0.5 * m_gravity * m_timeSinceShot * m_timeSinceShot;
 
-void CannonBall::customEvent(QEvent *event)
-{
-
+	setPos(x, y);
 }
